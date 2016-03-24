40fabd47fa9821a77c676aa6ff0f6c707afdc24c
Markus Unterwaditzer <markus@unterwaditzer.net>
2016-03-25 00:03:31 +0100

    Clippy leftovers

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,5 @@
 // DOCS
 
-#![cfg_attr(feature = "clippy", allow(unstable_features))]
-#![cfg_attr(feature = "clippy", feature(plugin))]
-#![cfg_attr(feature = "clippy", plugin(clippy))]
-#![cfg_attr(feature = "clippy", deny(warnings))]
-
 extern crate iron;
 
 use iron::prelude::*;
