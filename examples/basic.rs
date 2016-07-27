2effdafb06181971a691877f12630bb01966d3e3
Markus Unterwaditzer <markus@unterwaditzer.net>
2016-07-27 02:18:22 +0200

    Improve examples

--- examples/basic.rs
+++ examples/basic.rs
@@ -12,14 +12,29 @@ fn main() {
     let mut chain = Chain::new(handler);
     chain.link_after({
         let mut error_router = iron_error_router::ErrorRouter::new();
+
+        // In case the response is a 404, replace the response with your own.
+
         error_router.handle_status(status::NotFound, |_: &mut Request| {
-            // If you need to use the original response, you can also register an AfterMiddleware
-            // using `error_router.after_status`.
             Ok(Response::with((
                 status::NotFound,
-                "Content not found. Check if you have an internet connection."  // Totally useful error page that is not irritating anybody.
+                "Content not found."
             )))
         });
+
+        // Instead of writing a handler, you can just use a modifier:
+
+        error_router.modifier_for_status(
+            status::NotFound,
+            (status::NotFound, "Content not found.")
+        );
+
+        // Or an AfterMiddleware:
+
+        error_router.after_status(status::NotFound, |_: &mut Request, _: &mut Response| {
+            Ok(Response::with((status::NotFound, "Content not found.")))
+        });
+
         error_router
     });
 
